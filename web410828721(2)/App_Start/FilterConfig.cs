7d2deb36fe000be5897c3d2230d14d81c6cbd3cb
s1082872 <uuu9147096@gmail.com>
2021-05-06 19:12:30 +0800

    first commit

---
+++ web410828721(2)/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace web410828721_2_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ web410828721(2)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web410828721_2_.MvcApplication" Language="C#" %>
