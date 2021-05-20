79fc21b9905ee9e492b198c8714ed7c68e23fe50
Eveon <s1082872@gm.pu.edu.tw>
2021-05-20 20:38:54 +0800

    hw2

---
+++ web410828721(2)/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web410828721_2_.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult HW2()
+        {
+            return View();
+        }
+    }
+}
